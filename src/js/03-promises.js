f8a610e884c93f8c54dfbbb78419d53810410848
ilusha <ilya.yakov.com@gmail.com>
2022-06-25 16:38:16 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,16 +5,16 @@ const step = document.querySelector('input[name=step]');
 const amount = document.querySelector('input[name=amount]');
 const form = document.querySelector('.form');
 // position, delay
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   } else {
-//     // Reject
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   }
-// }
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  } else {
+    // Reject
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  }
+}
 function repetable() {
   // for(let i = 0; i<parseInt(amount.value) ;i++){
   // const special = (parseInt(delay.value) + parseInt(step.value)*i)
