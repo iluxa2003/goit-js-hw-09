5bdefa989e747fae7a201afb7ec2263092442a9a
ilusha <ilya.yakov.com@gmail.com>
2022-06-25 16:34:36 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,9 @@
 import Notiflix from 'notiflix';
-const subButton = document.querySelector("button[type=submit]")
-const delay = document.querySelector("input[name=delay]")
-const step = document.querySelector("input[name=step]")
-const amount = document.querySelector("input[name=amount]")
-const form = document.querySelector('.form')
+const subButton = document.querySelector('button[type=submit]');
+const delay = document.querySelector('input[name=delay]');
+const step = document.querySelector('input[name=step]');
+const amount = document.querySelector('input[name=amount]');
+const form = document.querySelector('.form');
 // position, delay
 // function createPromise(position, delay) {
 //   const shouldResolve = Math.random() > 0.3;
@@ -15,22 +15,29 @@ const form = document.querySelector('.form')
 //     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 //   }
 // }
-function repetable(){
+function repetable() {
   // for(let i = 0; i<parseInt(amount.value) ;i++){
-    const special = (parseInt(delay.value) + parseInt(step.value)*i)
+  // const special = (parseInt(delay.value) + parseInt(step.value)*i)
   //   // const special = delay.value + step.value*i
   //   setTimeout(createPromise,special,i+1, special)
   // }
-  createPromise(position, special)
-  .then(({ position, delay }) => {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
+  for (let i = 0; i < parseInt(amount.value); i++) {
+    const special = parseInt(delay.value) + parseInt(step.value) * i;
+    createPromise(position, special)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
 }
-const  starting = event => {
+const starting = event => {
   event.preventDefault();
   repetable();
-}
-form.addEventListener("submit",starting)
+};
+form.addEventListener('submit', starting);
