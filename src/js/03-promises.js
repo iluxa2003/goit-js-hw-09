ec7bef603adc77e7b70b919ba8252eacc710d170
ilusha <ilya.yakov.com@gmail.com>
2022-06-25 16:11:57 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,12 +16,18 @@ function createPromise(position, delay) {
   }
 }
 function repetable(){
-  for(let i = 0; i<parseInt(amount.value) ;i++){
-    const special = (parseInt(delay.value) + parseInt(step.value)*i)
-    // const special = delay.value + step.value*i
-    setTimeout(createPromise,special,i+1, special)
-  }
-
+  // for(let i = 0; i<parseInt(amount.value) ;i++){
+  //   const special = (parseInt(delay.value) + parseInt(step.value)*i)
+  //   // const special = delay.value + step.value*i
+  //   setTimeout(createPromise,special,i+1, special)
+  // }
+  createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
 }
 const  starting = event => {
   event.preventDefault();
