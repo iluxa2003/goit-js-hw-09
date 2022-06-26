cf39ac06927aa997cb50ba229b9ac6094f910548
ilusha <ilya.yakov.com@gmail.com>
2022-06-26 16:36:16 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -7,34 +7,41 @@ const form = document.querySelector('.form');
 // position, delay
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    // Reject
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
+  // if (shouldResolve) {
+  //   // Fulfill
+  //   Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  // } else {
+  //   // Reject
+  //   Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  // }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(value => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
 function repetable() {
-  for(let i = 0; i<parseInt(amount.value) ;i++){
-  const special = (parseInt(delay.value) + parseInt(step.value)*i)
-    // const special = delay.value + step.value*i
-    setTimeout(createPromise,special,i+1, special)
-  }
-  // for (let i = 0; i < parseInt(amount.value); i++) {
-  //   const special = parseInt(delay.value) + parseInt(step.value) * i;
-  //   createPromise(i+1, special)
-  //     .then(({position, delay }) => {
-  //       Notiflix.Notify.success(
-  //         `✅ Fulfilled promise ${position} in ${delay}ms`
-  //       );
-  //     })
-  //     .catch(({ position, delay }) => {
-  //       Notiflix.Notify.failure(
-  //         `❌ Rejected promise ${position} in ${delay}ms`
-  //       );
-  //     });
+  // for(let i = 0; i<parseInt(amount.value) ;i++){
+  // const special = (parseInt(delay.value) + parseInt(step.value)*i)
+  //   // const special = delay.value + step.value*i
+  //   setTimeout(createPromise,special,i+1, special)
   // }
+  for (let i = 0; i < parseInt(amount.value); i++) {
+    const special = parseInt(delay.value) + parseInt(step.value) * i;
+    createPromise(i + 1, special);
+  }
 }
 const starting = event => {
   event.preventDefault();
